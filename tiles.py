44725c23013eb5d75dd6b15e558dd55125ef9d02
Noaal <noaal6@gmail.com>
2015-05-18 19:10:31 +0200

    Added lockable doors and teleport features

--- tiles.py
+++ tiles.py
@@ -1,7 +1,7 @@
-
 import items
 from colors import Colors
-
+from actions import ActionException
+from items import Key
 
 class Tile:
     def __init__(self, y, x):
@@ -23,17 +23,34 @@ class Floor(Tile):
     def glyph(self):
         return self.item.glyph() if self.item else ('.', Colors.DARK_GRAY)
 
+class KeyFloor(Floor):
+    def __init__(self, y, x):
+        super().__init__(y,x)
+        self.item = Key()
+
 
 class Wall(Tile):
     def passable(self):
         return False
 
     def glyph(self):
-        return ('#', Colors.DARK_GRAY)
+        return ('#', Colors.WHITE)
 
     def __str__(self):
         return "wall"
 
+class Teleport(Tile):
+    def passable(self):
+        return True
+
+    def glyph(self):
+        return ('T', Colors.DARK_RED)
+
+    def __str__(self):
+        return "teleport"
+
+    def open(self):
+        return "you used the teleport"
 
 class Door(Tile):
     OPEN = 1
@@ -70,13 +87,35 @@ class Door(Tile):
         if isinstance(item, Knife):
             return "you stab at the door, leaving a mark"
 
+class KeyDoor(Door):
+    LOCKED = 3
+    def __init__(self, y, x):
+        super().__init__(y,x)
+        self.state = KeyDoor.LOCKED
+
+    def glyph(self):
+        return('/' if self.passable() else 'D', Colors.YELLOW)
+
+    #def open(self):
+    #     if self.state == KeyDoor.OPEN:
+    #         self.state = KeyDoor.CLOSED
+    #     else:
+    #         raise ActionException("do you have a key?")
+
+    def use(self, item):
+        if isinstance(item, Key):
+            self.state = KeyDoor.CLOSED
+            return "you opended the door"
+
+
 class TileFactory:
     TILES = {
         '.': Floor,
         '#': Wall,
         '+': Door,
-        '1': Floor,
-        '2': Floor,
+        '1': KeyDoor,
+        '2': KeyFloor,
+        '3': Teleport,
     }
 
     @staticmethod
